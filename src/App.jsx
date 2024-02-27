5bba0ba993af09e6482b8d2ac76a40a647506865
suzinxix <suzinxix@gmail.com>
2024-02-27 11:40:58 +0900

    feat: React-Query 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Global } from '@emotion/react';
 import Routers from './Routers/Routers';
 import reset from './styles/reset';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <div>
+  <QueryClientProvider client={queryClient}>
     <Global styles={reset} />
     <Routers />
-  </div>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
 
 export default App;
