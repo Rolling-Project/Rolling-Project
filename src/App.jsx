a69978730d78a6599af9c408165a5514e0e3267b
Sujeong Kim <lewis14081626@gmail.com>
2024-02-27 16:37:25 +0900

    init: 프로젝트 초기 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { Global } from '@emotion/react';
-import styled from '@emotion/styled';
 import reset from './styles/reset';
-import colors from './styles/colors';
-
-// color variant 사용 예시
-const Button = styled.button`
-  background-color: ${colors['Blue-100']};
-  color: ${colors.Black};
-`;
+import createCard from './components/create/createCard';
 
 function App() {
   return (
-    <div>
+    <>
       <Global styles={reset} />
-      <Button>This is a hotpink button.</Button>
-    </div>
+      <createCard />
+    </>
   );
 }
 
