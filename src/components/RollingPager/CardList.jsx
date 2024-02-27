6963fc6f524fc0eca775e8c9dd34ce7eed575536
suzinxix <suzinxix@gmail.com>
2024-02-27 11:50:32 +0900

    feat: 생성된 롤링페이퍼 페이지의 카드 리스트 컴포넌트 UI 구현

---
+++ src/components/RollingPager/CardList.jsx
@@ -0,0 +1,41 @@
+import { useQuery } from '@tanstack/react-query';
+import styled from '@emotion/styled';
+import MessageCard from './MessageCard';
+import PlusCard from './PlusCard';
+
+const List = styled.ul`
+  width: 1200px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+`;
+
+const CardList = () => {
+  const recipientId = 2687;
+
+  const fetchMessages = async () => {
+    const response = await fetch(`https://rolling-api.vercel.app/4-18/recipients/${recipientId}/messages/`);
+    const result = await response.json();
+    return result;
+  };
+
+  const { data, isLoading, error } = useQuery(['messages', recipientId], () => fetchMessages(recipientId));
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <List>
+        <PlusCard />
+        {data.results.map((person) => (
+          <li key={person.id}>
+            <MessageCard person={person} />
+          </li>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default CardList;
--- src/pages/RollingPaper.jsx
+++ src/pages/RollingPaper.jsx
@@ -1,36 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
-import styled from '@emotion/styled';
-import MessageCard from '../components/RollingPager/MessageCard';
-import PlusCard from '../components/RollingPager/PlusCard';
-
-const CardList = styled.ul``;
+import CardList from '../components/RollingPager/CardList';
 
 const RollingPaper = () => {
-  const recipientId = 2687;
-
-  const fetchMessages = async () => {
-    const response = await fetch(`https://rolling-api.vercel.app/4-18/recipients/${recipientId}/messages/`);
-    const result = await response.json();
-    return result;
-  };
-
-  const { data, isLoading, error } = useQuery(['messages', recipientId], () => fetchMessages(recipientId));
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  return (
-    <div>
-      <PlusCard />
-      <CardList>
-        {data.results.map((person) => (
-          <li key={person.id}>
-            <MessageCard person={person} />
-          </li>
-        ))}
-      </CardList>
-    </div>
-  );
+  return <CardList />;
 };
 
 export default RollingPaper;
