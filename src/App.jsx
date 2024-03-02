7f3f9831f77878082f80107748c3ed04fa8ebab2
Sujeong Kim <lewis14081626@gmail.com>
2024-03-02 21:36:04 +0900

    fix: eslint 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { Global } from '@emotion/react';
 import reset from './styles/reset';
 import createCard from './components/create/createCard';
 
-function App() {
-  return (
-    <>
-      <Global styles={reset} />
-      <createCard />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Global styles={reset} />
+    <createCard />
+  </>
+);
 
 export default App;
