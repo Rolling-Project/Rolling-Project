3dbb137d04e93a11927cfc6f826b8a83c83570c2
suzinxix <suzinxix@gmail.com>
2024-03-07 19:04:19 +0900

    fix: 롤링페이퍼 삭제 버튼 위치 수정

--- src/pages/RollingPaper.jsx
+++ src/pages/RollingPaper.jsx
@@ -1,13 +1,13 @@
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useLocation, useParams } from 'react-router-dom';
 import styled from '@emotion/styled';
 import CardList from '../components/RollingPager/CardList';
 import MessageModal from '../components/RollingPager/Modal';
 import useModal from '../utils/hooks/useModal';
 import useGetMessages from '../utils/hooks/useGetMessages';
-import { useInView } from 'react-intersection-observer';
-import { useEffect } from 'react';
 import Header from '../components/RollingPager/Header';
 import colors from '../styles/colors';
-import { useLocation, useParams } from 'react-router-dom';
 import useDeleteRollingPaper from '../utils/hooks/useDeleteRollingPaper';
 import { Primary40Button } from '../components/common/Button/Button';
 
@@ -25,15 +25,21 @@ const Content = styled.div`
   padding: 246px 24px;
 `;
 
-const DeletedButton = styled.div`
-  margin-left: auto;
+const DeletedSection = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  justify-content: flex-end;
   margin-bottom: 11px;
 
-  @media (max-width: 820px) {
-    position: fixed;
-    bottom: 25px;
-    width: 100%;
-    max-width: calc(100vw - 24px * 2);
+  div {
+    width: fit-content;
+    @media (max-width: 820px) {
+      position: fixed;
+      bottom: 25px;
+      width: 100%;
+      max-width: calc(100vw - 24px * 2);
+    }
   }
 `;
 
@@ -64,9 +70,11 @@ function RollingPaper() {
     <Container>
       <Header messages={data?.pages} />
       <Content>
-        <DeletedButton>
-          {isEdit && <Primary40Button onClick={() => handleDelete({ id: recipientId })}>삭제하기</Primary40Button>}
-        </DeletedButton>
+        <DeletedSection>
+          <div>
+            {isEdit && <Primary40Button onClick={() => handleDelete({ id: recipientId })}>삭제하기</Primary40Button>}
+          </div>
+        </DeletedSection>
         <CardList messages={data?.pages.map((page) => page.result).flat()} onClick={openModal} lastRef={ref} />
         {isModalOpen && <MessageModal message={clickedItem} onClose={closeModal} />}
       </Content>
