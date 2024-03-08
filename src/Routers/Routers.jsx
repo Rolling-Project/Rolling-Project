5ed59e8209ebfc13115b7201a2a2388798935675
suzinxix <suzinxix@gmail.com>
2024-03-08 11:10:09 +0900

    fix: Router에 헤더 추가

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -2,16 +2,18 @@ import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import AllListPage from '../pages/AllListPage/AllListPage';
 import ListPage from '../pages/ListPage/ListPage';
 import RollingPaper from '../pages/RollingPaper';
+import Header from '../components/common/Header/Header';
 
 function Routers() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/list" element={<ListPage />} />
         <Route path="/list/all" element={<AllListPage />} />
         <Route path="/post/:id" element={<RollingPaper />} />
         <Route path="/post/:id/edit" element={<RollingPaper />} />
-    </Routes>
+      </Routes>
     </Router>
   );
 }
