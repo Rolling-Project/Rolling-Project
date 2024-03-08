1c954c039c235c0fd1533d97db1ccf77380a12e7
Sujeong Kim <lewis14081626@gmail.com>
2024-03-08 15:43:02 +0900

    refactor: merge 전 동기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Global } from '@emotion/react';
-import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter 임포트
 import Routers from './Routers/Routers';
 import reset from './styles/reset';
 
@@ -10,11 +9,9 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <Global styles={reset} />
-        <Routers />
-        <ReactQueryDevtools />
-      </Router>
+      <Global styles={reset} />
+      <Routers />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
