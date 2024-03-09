49a709066eb70b1105f5ea7b26eb933d483a1ab1
Sujeong Kim <lewis14081626@gmail.com>
2024-03-10 03:02:55 +0900

    refactor: 전체 컴포넌트 크기 50%로 변경

--- src/common/Button/ToggleButton.styled.js
+++ src/common/Button/ToggleButton.styled.js
@@ -3,15 +3,15 @@ import styled from '@emotion/styled';
 import colors from '../../styles/colors';
 
 export const ToggleDiv = styled.div`
-  width: 24.4rem;
-  height: 4rem;
+  width: 12.2rem;
+  height: 2rem;
   display: flex;
   border-radius: 0.6rem;
   background: ${colors['--Gray-100']};
 `;
 
 export const Toggle = styled.div`
-  width: 12.2rem;
+  width: 6.1rem;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -20,7 +20,7 @@ export const Toggle = styled.div`
   border: 2px solid ${({ $isToggled }) => ($isToggled ? colors['--Purple-600'] : `transparent`)};
   background: ${({ $isToggled }) => ($isToggled ? colors['--White'] : colors['--Gray-100'])};
 
-  font-size: 1.6rem;
+  font-size: 0.8rem;
   font-style: normal;
   line-height: 2.6rem; /* 162.5% */
   font-weight: ${({ $isToggled }) => ($isToggled ? `700` : `400`)};
