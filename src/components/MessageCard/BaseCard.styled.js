73a3af78a164e4200728783c3e2d8e6d0c8284d3
suzinxix <suzinxix@gmail.com>
2024-03-15 09:12:33 +0900

    refactor: 메세지 카드 컴포넌트 수정

--- src/components/MessageCard/BaseCard.styled.js
+++ src/components/MessageCard/BaseCard.styled.js
@@ -5,8 +5,13 @@ const BaseCard = styled.li`
   height: 17.5rem;
   border-radius: 1rem;
   background: ${colors['--White']};
-  box-shadow: 0 .125rem .75rem 0 rgba(0, 0, 0, 0.08);
+  box-shadow: 0 0.12rem 0.75rem 0 rgba(0, 0, 0, 0.08);
   cursor: pointer;
+
+  &:hover {
+    transition: 0.5s;
+    transform: scale(1.05);
+  }
 `;
 
 export default BaseCard;
